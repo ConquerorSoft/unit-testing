681d7e7da85ffa6c8b971fb4eb5c5c54708a3078
Christian Varela <dobleclick.mx@gmail.com>
2020-01-16 19:46:21 -0500

    PSR-12

--- src/StringTransformation.php
+++ src/StringTransformation.php
@@ -4,10 +4,10 @@ namespace conquerorsoft\unit_testing;
 
 class StringTransformation
 {
-    const ALPHA_DECODED = "abcdefghijklmnopqrstuvwxyz ";
-    const ENCODED = ['å','Å','ç','ı','´','ƒ','©','˙','ˆ','Ç','˚','¬','µ','˜','ø','π','œ','®','ß','Î','¨','¡','Ï','Ó','¥','Ω','  '];
+    protected const ALPHA_DECODED = "abcdefghijklmnopqrstuvwxyz ";
+    protected const ENCODED = ['å','Å','ç','ı','´','ƒ','©','˙','ˆ','Ç','˚','¬','µ','˜','ø','π','œ','®','ß','Î','¨','¡','Ï','Ó','¥','Ω','  '];
     
-    public function encode(string $string):string
+    public function encode(string $string): string
     {
         $string_to_return = "";
         for ($i = 0; $i < strlen($string); $i++) {
@@ -17,10 +17,10 @@ class StringTransformation
         return $string_to_return;
     }
     
-    public function decode(string $string):string
+    public function decode(string $string): string
     {
         $string_to_return = "";
-        for ($i = 0; $i < strlen($string); $i+=2) {
+        for ($i = 0; $i < strlen($string); $i += 2) {
             $pos = array_search(substr($string, $i, 2), self::ENCODED);
             $string_to_return .= self::ALPHA_DECODED[$pos];
         }
--- tests/StringTransformationTest.php
+++ tests/StringTransformationTest.php
@@ -9,14 +9,16 @@ class StringTransformationTest extends TestCase
     /**
      * @dataProvider alphaValidStringsProvider
      */
-    public function testAlphaValidStringIsEncoded(string $input_string, string $expected)
-    {
+    public function testAlphaValidStringIsEncoded(
+        string $input_string,
+        string $expected
+    ) {
         $string = new StringTransformation();
         $new_string = $string->encode($input_string);
         $this->assertEquals($expected, $new_string);
     }
     
-    public function alphaValidStringsProvider():array
+    public function alphaValidStringsProvider(): array
     {
         return [
             ['some string', 'ßøµ´  ßÎ®ˆ˜©'],
@@ -29,14 +31,16 @@ class StringTransformationTest extends TestCase
     /**
      * @dataProvider encodedValidStringsProvider
      */
-    public function testEncodedValidStringIsDecoded(string $input_string, string $expected)
-    {
+    public function testEncodedValidStringIsDecoded(
+        string $input_string,
+        string $expected
+    ) {
         $string = new StringTransformation();
         $new_string = $string->decode($input_string);
         $this->assertEquals($expected, $new_string);
     }
 
-    public function encodedValidStringsProvider():array
+    public function encodedValidStringsProvider(): array
     {
         return [
             ['πåßßÏø®ı', 'password'],
